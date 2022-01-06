1e3393cf18fbe13d60b8211bc9b949b88acb0f7a
Glenn Posadas <hello@glennvon.com>
2022-01-06 14:59:20 +0800

    add assignment and swift stuff layout.

--- SwiftCodeUI/SwiftCodeUI/ViewController.swift
+++ SwiftCodeUI/SwiftCodeUI/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
+// ASSIGNMENT:
+// 1. swift optional chaining, optional unwrapping.
+// 2. COCOAPODS
+// 3. make a swift app, with 2 screens.
+// 3.a -> first screen has email field, and password protected field, and name field, and login button.
+// 3.b -> make a "WELCOME + name" in the second screen.
+
 class ViewController: UIViewController {
 
+  var label: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    // Set the bg color of our super view. or parent view.
+    view.backgroundColor = .white
+    
+    // Without autolayout/constraint.
+    let frame = CGRect(x: 183218412, y: 432432432, width: 300, height: 50)
+    let centerPointOfOurParentSuperView = CGPoint(x: view.center.x, y: view.center.y)
+    
+    label = UILabel(frame: frame)
+    label.center = centerPointOfOurParentSuperView
+    
+    label.text = "Hello, world by code!"
+    label.textColor = .darkGray
+    label.textAlignment = .center
+    //label.backgroundColor = .red
+    
+    view.addSubview(label)
   }
-
-
 }
 
